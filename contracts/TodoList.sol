bb06fb22e6347dc070dfc1b72ed093e83d7eae62
Simranjeet Singh <smrnjeet222@gmail.com>
2021-06-17 10:38:52 +0530

    task contract complete

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -10,14 +10,33 @@ contract TodoList {
     bool completed;
   }
 
-  constructor() {
+  constructor() public{
     createTask('Example Task Intialized');
   }
 
   mapping(uint => Task) public tasks;
 
+  event TaskCreated(
+    uint id,
+    string content, 
+    bool completed
+  );
+
+  event TaskCompleted(
+    uint id,
+    bool completed
+  );
+
   function createTask(string memory _content) public {
-    tasks[taskCount] = Task(taskCount, _content, false);
     taskCount++;
+    tasks[taskCount] = Task(taskCount, _content, false);
+    emit TaskCreated(taskCount, _content, false);
+  }
+
+  function toggleCompleted(uint _id) public {
+    Task memory _task = tasks[_id];
+    _task.completed = !_task.completed;
+    tasks[_id] = _task;
+    emit TaskCompleted(_id, _task.completed);
   }
 }
