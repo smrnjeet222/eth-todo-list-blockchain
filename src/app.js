5aa26d544df1bd34a2500cf3e334c1f2baa05a98
Simranjeet Singh <smrnjeet222@gmail.com>
2021-06-17 10:39:12 +0530

    ui

--- src/app.js
+++ src/app.js
@@ -3,95 +3,110 @@ App = {
   contracts: {},
 
   load: async () => {
-    await App.loadAccount()
-    await App.loadContract()
-    await App.render()
+    await App.loadAccount();
+    await App.loadContract();
+    await App.render();
   },
 
   loadAccount: async () => {
     const accounts = await ethereum.request({ method: 'eth_accounts' });
-    App.account = accounts[0]
+    App.account = accounts[0];
     console.log(accounts[0]);
   },
 
   loadContract: async () => {
-    const todoList = await $.getJSON('TodoList.json')
+    const todoList = await $.getJSON('TodoList.json');
 
-    App.contracts.TodoList = TruffleContract(todoList)
-    App.contracts.TodoList.setProvider(window.ethereum)
+    App.contracts.TodoList = TruffleContract(todoList);
+    App.contracts.TodoList.setProvider(window.ethereum);
 
     // Hydrate the smart contract with values from the blockchain
-    App.todoList = await App.contracts.TodoList.deployed()
+    App.todoList = await App.contracts.TodoList.deployed();
   },
 
   render: async () => {
     // Prevent double render
     if (App.loading) {
-      return
+      return;
     }
 
     // Update app loading state
-    App.setLoading(true)
+    App.setLoading(true);
 
     // Render Account
-    $('#account').html(App.account)
+    $('#account').html(App.account);
 
     // Render Tasks
-    await App.renderTasks()
+    await App.renderTasks();
 
     // Update loading state
-    App.setLoading(false)
+    App.setLoading(false);
   },
 
   renderTasks: async () => {
     // Load the total task count from the blockchain
-    const taskCount = await App.todoList.taskCount()
-    const $taskTemplate = $('.taskTemplate')
+    const taskCount = await App.todoList.taskCount();
+    const $taskTemplate = $('.taskTemplate');
 
     // Render out each task with a new task template
     for (var i = 1; i <= taskCount; i++) {
       // Fetch the task data from the blockchain
-      const task = await App.todoList.tasks(i)
-      const taskId = task[0].toNumber()
-      const taskContent = task[1]
-      const taskCompleted = task[2]
+      const task = await App.todoList.tasks(i);
+      const taskId = task[0].toNumber();
+      const taskContent = task[1];
+      const taskCompleted = task[2];
 
       // Create the html for the task
-      const $newTaskTemplate = $taskTemplate.clone()
-      $newTaskTemplate.find('.content').html(taskContent)
+      const $newTaskTemplate = $taskTemplate.clone();
+      $newTaskTemplate.find('.content').html(taskContent);
       $newTaskTemplate.find('input')
                       .prop('name', taskId)
                       .prop('checked', taskCompleted)
-                      // .on('click', App.toggleCompleted)
+                      .on('click', App.toggleCompleted);
 
       // Put the task in the correct list
       if (taskCompleted) {
-        $('#completedTaskList').append($newTaskTemplate)
+        $('#completedTaskList').prepend($newTaskTemplate);
       } else {
-        $('#taskList').append($newTaskTemplate)
+        $('#taskList').prepend($newTaskTemplate);
       }
 
       // Show the task
-      $newTaskTemplate.show()
+      $newTaskTemplate.show();
     }
   },
 
+  createTask: async () => {
+    App.setLoading(true);
+    const content = $('#newTask').val();
+    await App.todoList.createTask(content, {from: App.account });
+    window.location.reload();
+  },
+
+  toggleCompleted: async (e) => {
+    App.setLoading(true);
+    const taskId = e.target.name;
+    console.log(taskId);
+    await App.todoList.toggleCompleted(taskId, {from: App.account });
+    window.location.reload();
+  },
+
   setLoading: (boolean) => {
-    App.loading = boolean
-    const loader = $('#loader')
-    const content = $('#content')
+    App.loading = boolean;
+    const loader = $('#loader');
+    const content = $('#content');
     if (boolean) {
-      loader.show()
-      content.hide()
+      loader.show();
+      content.hide();
     } else {
-      loader.hide()
-      content.show()
+      loader.hide();
+      content.show();
     }
   }
 }
 
 $(() => {
   $(window).load(() => {
-    App.load()
+    App.load();
   })
 })
